fb412b058dbac37d2ea4549862192d24eade2d88
Jaron Cummings <jkc5020@rit.edu>
2020-10-20 20:48:25 -0400

    Initial commit

---
+++ server/src/test/java/com/GroceryStore/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroceryStore.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
